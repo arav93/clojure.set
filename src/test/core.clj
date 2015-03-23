32aa3e34e9df009c63d5436bdf79a26460836b09
Aravind K N <aravikn@gmail.com>
2015-03-23 17:26:50 +0530

    Update core.clj
    Changed the annotations for rename-keys and select.

--- src/test/core.clj
+++ src/test/core.clj
@@ -8,5 +8,6 @@
 (t/ann ^:no-check clojure.set/map-invert [(t/Map t/Any t/Any )  -> (t/Map t/Any t/Any)])
 (t/ann ^:no-check clojure.set/project [(t/Set t/Any) (t/Vec t/Any) -> (t/Set t/Any)])
 (t/ann ^:no-check clojure.set/rename [(t/Set t/Any) (t/Map t/Any t/Any) -> (t/Set t/Any)])
-(t/ann ^:no-check clojure.set/rename-keys [(t/Set t/Any) (t/Map t/Any t/Any) -> (t/Set t/Any)])
-(t/ann ^:no-check clojure.set/select [(t/Set t/Any) -> (t/Set t/Any)])
+(t/ann ^:no-check clojure.set/rename-keys [(t/Map t/Any t/Any) (t/Map t/Any t/Any) -> (t/Map t/Any t/Any)])
+(t/ann ^:no-check clojure.set/select [ (t/IFn [t/AnyInteger -> java.lang.Boolean]) (t/Set t/Any) -> (t/Set t/Any)])
+
